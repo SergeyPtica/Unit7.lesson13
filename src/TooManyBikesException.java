2b7905ab588f5cd7c9c727159f02cd9d352c70f4
Serg <s.ostafiichuk@gmail.com>
2017-04-04 22:43:16 +0300

    Initial commit

---
+++ src/TooManyBikesException.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Serg on 02.04.2017.
+ */
+
+public class TooManyBikesException extends Exception {
+
+    TooManyBikesException(String msgText)
+    {
+        super(msgText);
+    }
+}
+
